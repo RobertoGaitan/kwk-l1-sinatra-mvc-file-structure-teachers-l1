75261ccb5c7c1c486b529aa52332b6c805499370
Roberto Gaitan <rgaitan.it@gmail.com>
2018-07-18 15:14:50 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,38 @@
-class Model
+#class Model
   # Replace with Dog clas
+#end
+
+class Dog
+  
+  @@all = []
+  
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << breed
+  end
+  
+  def all
+    return @@all
+  end
+  
+  def name
+    @name
+  end
+  def breed
+    @breed
+  end
+  def age
+    @age
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  def age=(new_age)
+    @age = new_age
+  end
+
 end
+
